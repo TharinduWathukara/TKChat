f0b242c10e6d315332a2310c7af1734f04b22a31
TharinduWathukara <wtgwathukara@gmail.com>
2018-04-12 09:13:44 +0530

    Register Ok

--- app/src/main/java/com/example/tharindu/tkchat/MainActivity.java
+++ app/src/main/java/com/example/tharindu/tkchat/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.tharindu.tkchat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -10,6 +14,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("TK Chat");
     }
 
     @Override
@@ -27,9 +36,33 @@ public class MainActivity extends AppCompatActivity {
         //updateUI(currentUser);
 
         if(currentUser == null){
-            Intent startIntent = new Intent(MainActivity.this,StartActivity.class);
-            startActivity(startIntent);
-            finish();
+            sentTOStart();
         }
+    }
+
+    private void sentTOStart() {
+        Intent startIntent = new Intent(MainActivity.this,StartActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.main_logout_btn){
+            FirebaseAuth.getInstance().signOut();
+            sentTOStart();
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/tharindu/tkchat/RegisterActivity.java
+++ app/src/main/java/com/example/tharindu/tkchat/RegisterActivity.java
@@ -1,16 +1,18 @@
- package com.example.tharindu.tkchat;
+package com.example.tharindu.tkchat;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;  //
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;  //
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -47,7 +49,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void registerUser(String display_name,String email,String password){
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this,new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
